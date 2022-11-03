52c32f0eedf186188c51ef644005a7018e9cc8e1
Mozheykin <Mozheykin'@example.com>
2022-11-03 12:15:50 +0300

    new exe file, change encode with Window app

--- EXE/config.py
+++ EXE/config.py
@@ -1,2 +1,2 @@
-get_path = '/home/legal/.mt5/drive_c/Program Files/MetaTrader 5/MQL5/Files'
-put_path = '/home/legal/.wine/drive_c/Program Files/MetaTrader 5 Two/MQL5/Files'
+get_path = 'C:\\Users\\user\\AppData\\Roaming\\MetaQuotes\\Terminal\\D0E8209F77C8CF37AD8BF550E51FF075\\MQL5\\Files'
+put_path = 'C:\\Users\\user\\AppData\\Roaming\\MetaQuotes\\Terminal\\6898FBE063792F5CEA626E7ECE071114\\MQL5\\Files'
